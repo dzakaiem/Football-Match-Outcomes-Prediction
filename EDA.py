04778bb9d48b671f14c8ff30d1b18d275945a5b3
Daniel Zakaiem <danielzakaiem@yahoo.co.uk>
2022-01-25 12:12:52 +0000

    Neaten up code

--- EDA.py
+++ EDA.py
@@ -4,17 +4,14 @@ import numpy as np
 import matplotlib.pyplot as plt 
 import seaborn as sns 
 import os
-
 from sqlalchemy import true
 
-
-
 # step 1 - understanding the data
-# lets start with SOLELY using all the premier league data:
+# start with only data for premier league
 
 data_set_names = os.listdir('/Users/danielzakaiem/Downloads/Football-Dataset/premier_league') # list of premier league names
 print (data_set_names)
-data_set_names = sorted(data_set_names) #puts list of data set names in order (from 1990-2021)
+data_set_names = sorted(data_set_names) # puts list of data set names in order (from 1990-2021)
 my_list = [] 
 for data_set in data_set_names:
     my_list.append(pd.read_csv(f'/Users/danielzakaiem/Downloads/Football-Dataset/premier_league/{data_set}'))
@@ -25,26 +22,19 @@ print (whole_df.head())
 print(whole_df.tail())
 
 
-
-# step 2 - cleaning the data - I HAVE DECIDED TO MAKE IT SO THAT THEMODEL PREDICTS 'WILL THE RESULT BE 'H' (HOME TEAM), 'A'(AWAY TEAM') OR 'D'(DRAW)
+# step 2 - cleaning the data 
 
 irrelevant_features = ['League']
 for feature in irrelevant_features:
-    whole_df = whole_df.drop (feature, axis = 1) #drop irrelevant columns
-
+    whole_df = whole_df.drop (feature, axis = 1) # drop irrelevant columns
 
 whole_df ['Outcome'] = np.nan # make new column with null values
 
 whole_df['Index'] = np.arange(len(whole_df)) # add an index column 
 whole_df.set_index('Index') 
-
-
-
 print (whole_df.shape)
-whole_df = whole_df.drop(whole_df.index[12293]) # this should drop row with index 12293 (so now its missing from df) - '17 Jan' was written for the 'Result' - error in data
+whole_df = whole_df.drop(whole_df.index[12293]) # this should drop row with index 12293 (the 'error' row)
 print (whole_df.shape)  
-
-
 
 Home_goals = []
 Away_goals = []
@@ -71,15 +61,13 @@ for x in range(0, (len(whole_df)+1)):    # remember , row 12293 is non existant 
 
 
 
-#print (list_of_results) # now we need to add all these results into our new 'Outcome' column
+# now we need to add all these results into new 'Outcome' column
 
 whole_df['Outcome'] = list_of_results # add these 3 new columns
 whole_df ['Home_goals'] = Home_goals
 whole_df ['Away_goals'] = Away_goals
 
-
 print (whole_df.tail())
-
 
 df_saved_in_excel = whole_df.to_excel("ma_whole_df.xlsx", index = False)
 
@@ -87,17 +75,8 @@ df_saved_in_excel = whole_df.to_excel("ma_whole_df.xlsx", index = False)
 
 # step 3 - analysing relationship between variables ('features') - lets calculate the pearson correlation co-efficient between the variables to decdie on elimination?
 
-print (whole_df.corr()) # infers week correlation with all- so do i drop all the vairables? (ie: not use any of these features)
-#correlations_matrix =  (whole_df.corr()) #gives a matrix of ALL correlation co-efficients between each variable (mor eeffcient than dfinding them seperately no?)
-#print (correlations_matrix.head()) #I THINK IT ONLY SHOWS BETWEEN SEASON AND ROUND COLUMNS (VARIABLES) AS THESE ONLY WITH SAME OBJECT TYPE TO COMPARE 
+print (whole_df.corr()) 
 
-
-#first lets do analysis on 'season' and 'round' column:
-#print (np.corrcoef(whole_df['Season'],whole_df['Round'])) # this method gets the correlation co-efficient in matrix form- so the r value is just -0.047...
-
-#scatter_plot = sns.scatterplot(x = whole_df['Season'], y = whole_df ['Round']) # ovs basically shows the graph which has this specific 'r' value ^^^
-# not sure comand to show this object^
-#np.corrcoef(whole_df['Home_Team'], whole_df['Result']) # you cant find the correlation coefficient between a string column and an integer column  - this wont work 
 
 
 
--- New_features.py
+++ New_features.py
@@ -1,10 +1,5 @@
-# this file is solely to find new (potential) features to add to thhe previous:
-
-
-#'ELO' ranking for each match can be found on each link under 'ANALYSIS' section on the left box- scrpe this feature as well as other you can find:
-
-# from selenium import webdriver
-# driver = webdriver.Chrome()
+from selenium import webdriver
+driver = webdriver.Chrome()
 from collections import namedtuple
 from operator import index
 import pandas as pd
@@ -13,21 +8,19 @@ df = pd.read_excel('/Users/danielzakaiem/Desktop/Football-Match-Outcomes-Predict
 print (df.head())
 
 
-
-
-# to add ELO column to df:
+# add ELO column to df
 Home_ELO_list = []
 Away_ELO_list = []
 URL_list = list(df ['Link'].values.tolist())
 
 for URL in URL_list:
-    driver.get(URL)    # it seems cookies button only pops up for first one , not the rest after
+    driver.get(URL)    
     if URL == URL_list[0]:
         try:
             cookies_agree_button = driver.find_element_by_xpath('//*[@id="qc-cmp2-ui"]/div[2]/div/button[2]')
             print (f'This button says "{cookies_agree_button.text}", and I have just pressed it for you')
             cookies_agree_button.click()
-            ANALYSIS_button = driver.find_element_by_xpath('//*[@id="match"]/main/section[1]/div[1]/div/div/a[6]') #ANALYSIS BUTTON 
+            ANALYSIS_button = driver.find_element_by_xpath('//*[@id="match"]/main/section[1]/div[1]/div/div/a[6]') # analysis button
             ANALYSIS_button.click()
             Home_ELO = driver.find_element_by_xpath('//*[@id="mod_team_analysis"]/div/div/div/table/tbody/tr[2]/td[1]/span').text
             print (Home_ELO)
@@ -40,7 +33,7 @@ for URL in URL_list:
     
     elif URL != URL_list[0]:
         try:
-            ANALYSIS_button = driver.find_element_by_xpath('//*[@id="match"]/main/section[1]/div[1]/div/div/a[6]') #ANALYSIS BUTTON 
+            ANALYSIS_button = driver.find_element_by_xpath('//*[@id="match"]/main/section[1]/div[1]/div/div/a[6]') #analysis button
             ANALYSIS_button.click()
             Home_ELO = driver.find_element_by_xpath('//*[@id="mod_team_analysis"]/div/div/div/table/tbody/tr[2]/td[1]/span').text
             print (Home_ELO)
@@ -57,25 +50,14 @@ df ['Away_ELO'] = Away_ELO_list
 df_saved_in_excel = df.to_excel("newest_whole_df.xlsx", index = False)
 
 
-
-
-
-
-
 # next step would be to add new potential features: cumulative goals scored this season, result of previous match, 
 
 
-
-
-# goals so far:
-
-
-#make the column names for GSF:
+# add feature GSF (accumulated goals scored so far) - make the column names for GSF:
 
 teams = (df['Home_Team'].unique()) # gives list of all seperate teams 
 
 # make list for each team of "team-GSF"
-
 column_names = [] # for GSF of each team
 for team in teams:
     column_names.append(team + '_GSF') 
@@ -86,140 +68,23 @@ for name in column_names:
     print (type(name))
     actual_column_names.append(name)
 
-
-
-
-
-
-
-    
-    
-# actual_column_names = 
-
-# Charlton_Athletic_GSF 
-# Tottenham_Hotspur_GSF
-# Southampton_GSF
-# Sheffield_Wednesday_GSF
-# Queens_Park_Rangers_GSF
-# Nottingham_Forest_GSF
-# Man._Utd_GSF
-# Liverpool_GSF
-# Coventry_City_GSF
-# Wimbledon_FC_GSF
-# Arsenal_GSF
-# Millwall_GSF
-# Luton_Town_GSF
-# Everton_GSF
-# Crystal_Palace_GSF
-# Chelsea_GSF
-# Aston_Villa_GSF
-# Derby_County_GSF
-# Man._City_GSF
-# Norwich_City_GSF
-# Sheffield_United_GSF
-# Leeds_United_GSF
-# Sunderland_GSF
-# West_Ham_GSF
-# Notts_County_GSF
-# Oldham_Athletic_AFC_GSF
-# Ipswich_Town_GSF
-# Blackburn_Rovers_GSF
-# Middlesbrough_GSF
-# Newcastle_GSF
-# Swindon_Town_GSF
-# Leicester_GSF
-# Bolton_Wanderers_GSF
-# Barnsley_GSF
-# Watford_GSF
-# Bradford_City_GSF
-# Fulham_GSF
-# Birmingham_City_GSF
-# West_Bromwich_Albion_GSF
-# Portsmouth_GSF
-# Wolves_GSF
-# Wigan_Athletic_GSF
-# Reading_GSF
-# Hull_City_GSF
-# Stoke_City_GSF
-# Burnley_GSF
-# Blackpool_GSF
-# Swansea_City_GSF
-# Cardiff_City_GSF
-# AFC_Bournemouth_GSF
-# Brighton_&_Hove_Albion_GSF
-# Huddersfield_Town_GSF
-
-
-#STILL HAVENT FIGURED OUT HOW, BUT NEED TO MAKE THESE STRINGS^ INTO SEPERATE LISTS ( WHICH IS HARD COS U USUALLY ASSIGN A VARIABLE NAME TO = [], NOT A STRING )
-# SO COME BACK TO THIS LATER^
-
-# #lets try filling out one of these oclumns - solely for arsenal:
-# Ars_GSF = []  # once cell above is fixed, we can refer to this list and other team's lists in a for loop
-# home_team_column = list(df['Home_Team']) # strings
-# away_team_column = list(df ['Away_Team']) 
-# home_goals_column= list(df['Home_goals']) # integers
-# away_goals_column = list (df['Away_goals'])
-# data_on_each_row = (list(zip(home_team_column, away_team_column, home_goals_column, away_goals_column)))
-
-
-# 
-# for home_team, away_team, home_goals, away_goals in data_on_each_row:
-#     if home_team == 'Arsenal':
-#         if Ars_GSF[-1]:    # if there is already a stat for 'GOALS SO FAR', then we add to it (as it is cummulative). [-1] = 'last element in list'
-#             Ars_GSF.append(Ars_GSF[-1] + home_goals) # add previous number pf GSF (as its accumulated)
-#         else:
-#             Ars_GSF.append(home_goals) # if no value before, then add this as the 'first' value in the list
-#     elif away_team == 'Arsenal':
-#         if Ars_GSF[-1]:      # and then do exactly the same ^, but it is just for home goals (which doesnt make a difference- this is goals scored regardless of at home or away)
-#             Ars_GSF.append (Ars_GSF[-1] + away_goals)
-#         else:
-#             Ars_GSF.append(away_goals)
-#     else:
-#         if Ars_GSF: 
-#             Ars_GSF.append(Ars_GSF[-1]) # add the previous fig if nothing has been added to accumulation of goals
-#         else:
-#             Ars_GSF.append(0)
-            
-
-# df ['Arsenal_GSF'] = Ars_GSF # add accumulated goals so far for arsenal 
-# print (df.tail())
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-# below is more efficient than version above^. ascertain correctness, then eliminate code above
-
 home_team_column = list(df['Home_Team']) # strings
 away_team_column = list(df ['Away_Team']) 
 home_goals_column= list(df['Home_goals']) # integers
 away_goals_column = list (df['Away_goals'])
 data_on_each_row = (list(zip(home_team_column, away_team_column, home_goals_column, away_goals_column)))
 
-
-
-my_dict = { k: [] for k in teams} # now, add the list of accumulated goals to each of the values in this dictionary
+my_dict = { k: [] for k in teams} # key is the team, value is the list 
 print (my_dict)  
 for team , list in my_dict.items():
     for home_team, away_team, home_goals, away_goals in data_on_each_row:
         if home_team == team:
-            if list:    # if there is already a stat for 'GOALS SO FAR', then we add to it (as it is cummulative). [-1] = 'last element in list'
-                list.append(list[-1] + home_goals) # add previous number pf GSF (as its accumulated)
+            if list:    
+                list.append(list[-1] + home_goals) # cumulative, so must add to previous value for GSF
             else:
-                list.append(home_goals) # if no value before, then add this as the 'first' value in the list
+                list.append(home_goals) 
         elif away_team == team:
-            if list:      # and then do exactly the same ^, but it is just for home goals (which doesnt make a difference- this is goals scored regardless of at home or away)
+            if list:      # cumulative goals regardless of if scored home or away, so do same as above
                 list.append (list[-1] + away_goals)
             else:
                 list.append(away_goals)
@@ -230,11 +95,8 @@ for team , list in my_dict.items():
                 list.append(0)
 
 # the 'values' in this dictionary are the lists to fill the columns!
-
-
 for column_name, team in zip(actual_column_names,teams): #here, 'team' is essentially the key in the dictionary 
     df[column_name] = my_dict [team] 
-print (df.tail())
 
 
-
+df.to_excel("ma_whole_df_newest.xlsx", index = False) # new columns successfully added 
