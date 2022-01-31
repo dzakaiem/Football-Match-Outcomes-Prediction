971d48b905a5f747f8666643df57c8b7f3954b9e
Daniel Zakaiem <89412780+dzakaiem@users.noreply.github.com>
2022-01-31 13:58:40 +0000

    Update and rename machine_learning.py to Training_Model.py

--- Training_Model.py
+++ Training_Model.py
@@ -2,7 +2,8 @@ from outcome import Outcome
 import pandas as pd
 from sklearn import preprocessing
 import numpy as np
-from sklearn.metrics import accuracy_score, mean_squared_error
+import sklearn
+from sklearn.metrics import accuracy_score, mean_squared_error,r2_score
 from sklearn.model_selection import train_test_split 
 from sklearn.preprocessing import scale
 from sklearn.tree import DecisionTreeClassifier 
@@ -10,7 +11,7 @@ from sklearn.linear_model import LogisticRegression
 from sklearn.svm import SVC
 from sklearn.neighbors import KNeighborsClassifier
 import joblib
-
+from sklearn.model_selection import GridSearchCV
 
 df = pd.read_excel('/Users/danielzakaiem/Desktop/Football-Match-Outcomes-Prediction/ma_whole_df_newest.xlsx')
 print (df.head())
@@ -22,7 +23,7 @@ print (X.head())
 print (y.head())
 
 # we just standarize all column values into numbers (in order to apply machine learning to the dataset), EXCEPT from the target ('Outcome'), which stays categorical
-# 
+
 def preprocess_features(X):
     '''Converts all categorial variables into dummy varibles'''
     output = pd .DataFrame(index = X.index)
@@ -41,8 +42,6 @@ X_train, X_test, y_train, y_test = train_test_split(X,y,test_size=0.2)
 
 # first, standardize/normalize the data before feeding it to the model 
 
-# but wait, lets also create a validation set , as we want to compare different models
-# split the test test
 X_test, X_validation, y_test, y_validation = train_test_split (X_test, y_test, test_size=0.3)
 print ('Number of samples in:')
 print (f'Training: {len(y_train)}')
@@ -50,14 +49,13 @@ print (f'Validation: {len(y_validation)}')
 print (f'Testing: {len(y_test)}')
 
 # try mutiple sk-learn models
-# np.random.seed(2)
 
 models = [DecisionTreeClassifier(), SVC(), KNeighborsClassifier(n_neighbors=5)] 
 model_overview = []
 for model in models:
-    model.fit( X_train, y_train) # the model 
+    model.fit( X_train, y_train) 
 
-    y_test_pred = model.predict (X_test) # y prediction
+    y_test_pred = model.predict (X_test) 
     y_validation_prediction = model.predict (X_validation)
     
     test_loss = mean_squared_error (y_test , y_test_pred)
@@ -65,6 +63,7 @@ for model in models:
    
     test_accuracy = accuracy_score (y_test_pred, y_test)
     validaiton_accuracy = accuracy_score (y_validation, y_validation_prediction)
+
     
     model_overview.append(
      f"{model.__class__.__name__}: | Validation Loss: {validation_loss} | "
@@ -73,10 +72,34 @@ for model in models:
 
 print (model_overview)
 
-model = DecisionTreeClassifier().fit(X_train, y_train)
+model = DecisionTreeClassifier().fit(X_train, y_train) # best model 
 joblib.dump(model, "model.joblib")
 
-#WAIT, I THOUGHT YOU HAVETO USE VALIDATION SET TO COMPARE MODELS?
-# 'best' model is DecisionTreeClassifier - which we will use
+
+# tune the model using the validation dataset 
+
+search_space = {"criterion" : ["gini", "entropy"], "splitter" : ["best", "random"],"max_depth" : [1,2,3,4]} # search space 
+
+# make grid search object
+GS = GridSearchCV(estimator= model, 
+param_grid= search_space, 
+scoring= ["r2", "neg_root_mean_squared_error"], # will use these to score each model
+refit = "r2",              #the grid search onjec will return a modle that is best with respect to the r2 matric 
+cv = 5,                     # we wil be using 5 -fold cross vaidation 
+verbose = 4)              # this rells us how mich infrmation we want to print 
+
+GS.fit (X_train, y_train) # results of GS
+print (GS.best_estimator_) # complete details of the best model (ie: best set of hyperparams)
+print (GS.best_params_)  # to solely get best set of hyper params
+print(GS.best_score_) # according to what you used in the refit method (r2 in this case)
+
+# brings restults to CSV file to 'fully' reflect results 
+
+df = pd.DataFrame(GS.cv_results_)
+df = df.sort_values ("rank_test_r2")
+df.to_csv("cv_results.csv")
+
+# read over csv to come up woth a more efficient model - iterative process
+
 
 
